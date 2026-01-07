dfe1d46aade24c26c8700824d28e7441cc02ebea
Jun Kang <kangjun4092@gmail.com>
2026-01-07 23:57:56 +0900

    레이아웃수정 및 추가

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-//랜덤번호 지정
-//유저가 번호를 입력 GO라는 버튼을 누름
-//만약에 유저가 랜덤번호를 맞추면, 맞췄습니다.
-//랜덤번호  < 유저번호보다  down
-//랜덤번호 > 유저번호보다 up
-//Reset 버튼을 누르면 초기화
-//5번의 기회를 다쓰면 게임이 끝난다 ( 더이상 추측불가 버튼이 disable)
-//유저가 1~100 범위밖에 숫자를 입력하면 알려준다(기회를깎지않음)
-//유저가 이미 입력한 숫자를 입력하면 알려준다(기회를 깎지않음)
+
 
 let computerNum =0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
-let resultArea = document.getElementById("result-area");
-let resetButton = document.getElementById("reset-button")
-let chances = 5
-let gameover = false
+let resultText = document.querySelector(".result-text");
+let resetButton = document.querySelector("reset-button");
+let resultAreaImg = document.querySelector(".main-img");
+let chances = 5;
+let gameover = false;
 let chanceArea = document.getElementById("chance-area");
 let history=[];
 
-
+chanceArea.innerHTML = `남은 기회:${chances}`;
 playButton.addEventListener("click",play);
 resetButton.addEventListener("click",reset);
 userInput.addEventListener("focus", function(){userInput.value="";})
@@ -36,32 +29,34 @@ function play(){
     let userValue = userInput.value;
     
     if(userValue<1 || userValue>100){
-        resultArea.textContent = "1과 100사이 숫자를 입력해주세요."
+        resultText.textContent = "1과 100사이 숫자를 입력해주세요."
         return;
     }
 
     if(history.includes(userValue)){
-        resultArea.textContent = "이미 입력한 숫자입니다."
+        resultText.textContent = "이미 입력한 숫자입니다."
         return;
     }
 
     chances --;
-    chanceArea.textContent = `남은기회:${chances}번`;
+    chanceArea.innerHTML = `남은기회:${chances}`;
     console.log("chance",chances);
 
     if(userValue < computerNum){
-        resultArea.textContent = "UP";
+        resultAreaImg.src ="https://img.freepik.com/premium-vector/up-arrow-graphic-gradient-purple-blue-letter-u-p-isolated-vector-illustration_213497-3985.jpg?semt=ais_hybrid&w=740&q=80";
+        resultText.textContent = "UP";
     }else if(userValue > computerNum){
-        resultArea.textContent = "DOWN";
+        resultAreaImg.src ="https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/8d6498e9ca294113a12b95f294df0f6f?ik-sanitizeSvg=true"
+        resultText.textContent = "DOWN";
     }else{
-        resultArea.textContent = "정답";
+        resultText.textContent = "정답";
         gameover = true;
     }
 
     history.push(userValue);
     console.log(history);
 
-    if(chances < 1){
+    if(chances == 0){
         gameover = true;
     }
 
@@ -73,11 +68,13 @@ function play(){
 
 //리셋
 function reset(){
-    //user-input 창 클리어
+    
     userInput.value = "";
-    //새로운 번호 생성
+    resultAreaImg.src = "https://i.pinimg.com/236x/85/82/5c/85825c0d3b14e88b4823ff4456f34eeb.jpg";
     pickRandomNum();
-    resultArea.textContent = "결과값이 여기 나옵니다";
+    resultText.textContent = "결과값이 여기 나옵니다";
+    chances = 5;
+    chanceArea.innerHTML = `남은기회:${chances}`;
 }
 
 
